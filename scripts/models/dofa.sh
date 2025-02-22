75ea310c63a51079c83e84b6e31b6c2ed32c37cd
ando shah <ando@berkeley.edu>
2025-02-21 16:55:49 -0800

    eval config changes and dofa test script

--- scripts/models/dofa.sh
+++ scripts/models/dofa.sh
@@ -5,11 +5,19 @@ cmd='python geofm_src/main.py'
 
 ######## classification
 all_tasks=(
-    # 'base/dofa linear_probe corine_21'
+    'base/dofa linear_probe tropical_cyclone'
+    'base/dofa linear_probe corine_21'
+    'base/dofa linear_probe corine_sd'
     'base/dofa linear_probe fmow_8b'
     'base/dofa linear_probe fmow_4b'
-    # 'base/dofa linear_probe geobench_eurosat'
+    'base/dofa linear_probe geobench_eurosat_13b'
+    'base/dofa linear_probe hyperview_21b'
+    'base/dofa linear_probe digital_typhoon_3b'
+    'base/dofa linear_probe digital_typhoon_1b'
+    'base/dofa linear_probe geobench_eurosat_10b'
     )
+
+suffix='debug'
 
 for task in "${all_tasks[@]}"
 do
@@ -22,23 +30,24 @@ do
     model=$1
     training_mode=$2
     ds=$3
+    #        lr=0.002 \
 
     $cmd \
         model=$model \
         dataset=$ds \
+        output_dir=$ODIR/$model/$training_mode/$ds/$suffix \
         +model.training_mode=$training_mode \
         \
-        lr=0.002 \
         epochs=1 \
         warmup_epochs=0 \
         \
-        batch_size=512 \
+        batch_size=200 \
         num_workers=8 \
         num_gpus=1 \
         seed=13 \
         +trainer.fast_dev_run=True \
         \
-        # dataset.subset.train=0.4 # just as example how to use subset
+        dataset.subset.train=0.1 # just as example how to use subset
             
     # done
 done

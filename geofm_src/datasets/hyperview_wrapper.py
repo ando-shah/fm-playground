ba5ac1fcb2d5b92d7f73a799610c420b3c727f0e
ando shah <ando@berkeley.edu>
2025-02-26 10:29:14 -0800

    hyperview, TC updates for norming

--- geofm_src/datasets/hyperview_wrapper.py
+++ geofm_src/datasets/hyperview_wrapper.py
@@ -25,57 +25,59 @@ from .base_dataset import BaseDataset
 
 class HyperviewBenchmark(NonGeoDataset):
 
-    HYPERVIEW_MEAN = [ 290.7843,  290.3723,  286.6646,  292.1786,  299.0341,  312.8545,
-         320.6057,  321.0374,  322.5741,  324.4897,  328.3440,  334.4489,
-         339.1177,  351.7271,  363.4810,  369.6554,  377.4395,  385.3248,
-         393.0646,  393.4557,  399.7940,  411.9576,  430.1181,  450.2287,
-         461.3303,  468.1501,  477.8180,  483.6719,  492.9649,  499.4987,
-         508.5567,  515.3029,  520.2290,  522.4949,  522.2994,  522.7948,
-         528.0905,  531.5267,  537.5836,  544.4802,  551.8073,  555.8667,
-         559.2421,  561.5034,  566.9113,  572.5939,  575.4571,  574.9772,
-         577.4212,  580.6649,  585.6681,  589.2350,  591.1851,  591.1440,
-         594.5833,  598.5197,  602.8950,  614.3005,  619.6977,  617.0872,
-         611.0182,  603.3171,  598.6248,  597.0683,  600.6963,  595.6002,
-         592.6815,  596.1755,  604.9003,  630.3424,  664.4611,  703.5816,
-         742.1624,  775.1469,  801.4249,  825.0682,  856.0341,  884.6969,
-         913.3508,  942.0082,  970.6569,  999.3099, 1027.4458, 1056.8407,
-        1085.4407, 1114.2037, 1142.8521, 1169.8014, 1193.2913, 1212.1575,
-        1226.2242, 1236.3239, 1244.7190, 1250.3289, 1254.9497, 1264.5237,
-        1275.5308, 1287.0815, 1298.5231, 1308.8108, 1317.0856, 1322.9956,
-        1326.9932, 1330.5647, 1336.4053, 1341.7981, 1345.5804, 1350.1261,
-        1354.8489, 1359.4071, 1363.9717, 1368.5348, 1373.0947, 1377.6532,
-        1381.8234, 1387.7123, 1390.3658, 1395.1348, 1399.6979, 1407.9180,
-        1413.0568, 1404.8185, 1398.8970, 1402.4882, 1405.4640, 1407.9833,
-        1407.9244, 1408.6720, 1403.3954, 1400.0491, 1394.9823, 1400.9719,
-        1416.4968, 1423.0186, 1424.8911, 1429.3918, 1431.3179, 1433.4911,
-        1434.8601, 1435.8971, 1435.0978, 1436.8961, 1437.9344, 1439.0907,
-        1440.3364, 1441.6919, 1443.0356, 1443.9950, 1444.5940, 1445.2040]
-    
-    HYPERVIEW_STD = [ 184.6625,  184.4921,  182.4789,  185.6263,  189.6261,  197.5082,
-         202.0506,  202.4590,  203.6059,  205.0439,  207.6424,  211.5326,
-         214.5449,  222.0868,  229.2302,  233.0880,  237.8239,  242.5836,
-         247.2846,  247.8141,  251.8614,  259.4138,  270.4821,  282.6596,
-         289.5659,  293.8338,  299.7513,  303.4443,  309.1039,  313.0972,
-         318.5313,  322.6151,  325.6525,  327.1701,  327.2935,  327.8849,
-         331.3679,  333.7404,  337.6210,  341.9542,  346.5125,  349.0899,
-         351.2251,  352.6437,  355.9573,  359.4665,  361.3185,  361.1541,
-         362.8065,  364.9677,  368.1142,  370.2596,  371.3023,  371.0717,
-         372.9365,  375.1887,  377.9156,  385.1665,  388.9590,  387.9171,
-         384.7903,  380.7711,  378.7262,  378.7864,  382.1685,  379.7439,
-         379.1682,  382.6469,  388.4000,  402.2303,  419.0303,  437.7646,
-         457.3473,  476.8951,  496.5936,  518.0956,  545.7634,  574.8895,
-         606.0114,  638.8474,  673.1370,  708.6809,  744.6009,  782.8761,
-         821.8232,  861.3900,  900.5791,  936.6815,  966.9325,  989.7015,
-        1005.0406, 1014.6472, 1021.6771, 1026.5959, 1030.5853, 1038.7617,
-        1048.1011, 1057.2289, 1065.7727, 1072.9796, 1078.3110, 1081.6951,
-        1083.6781, 1085.4855, 1089.0480, 1092.2404, 1094.1437, 1096.6281,
-        1099.2626, 1101.7919, 1104.3225, 1106.8527, 1109.3929, 1111.9341,
-        1114.2084, 1117.8269, 1118.8195, 1121.4557, 1123.9249, 1129.6223,
-        1133.3379, 1126.6057, 1121.7826, 1124.5750, 1126.8206, 1128.5920,
-        1128.2804, 1128.6567, 1124.3521, 1121.5509, 1117.1182, 1121.2203,
-        1133.2728, 1138.4189, 1139.9752, 1143.5162, 1144.5846, 1145.2258,
-        1144.5868, 1143.1881, 1140.1182, 1139.0898, 1137.4788, 1135.8882,
-        1134.3488, 1132.7020, 1130.7249, 1128.3368, 1125.5746, 1122.6283]
+    # Calculated over 100% of train+val
+    HYPERVIEW_MEAN = [ 357.3309,  356.8241,  352.2739,  359.0434,  367.4655,  384.4400,
+         393.9638,  394.4952,  396.3852,  398.7393,  403.4731,  410.9720,
+         416.7098,  432.1976,  446.6424,  454.2320,  463.8037,  473.5013,
+         483.0237,  483.5212,  491.3178,  506.2690,  528.5842,  553.2881,
+         566.9301,  575.3107,  587.1912,  594.3837,  605.8002,  613.8228,
+         624.9483,  633.2333,  639.2795,  642.0591,  641.8145,  642.4167,
+         648.9135,  653.1238,  660.5588,  669.0240,  678.0240,  683.0078,
+         687.1527,  689.9290,  696.5694,  703.5454,  707.0600,  706.4673,
+         709.4718,  713.4590,  719.6038,  723.9843,  726.3781,  726.3223,
+         730.5421,  735.3784,  740.7471,  754.7546,  761.3821,  758.1700,
+         750.7112,  741.2469,  735.4827,  733.5818,  738.0514,  731.8038,
+         728.2272,  732.5236,  743.2369,  774.4900,  816.4062,  864.4771,
+         911.8911,  952.4366,  984.7453, 1013.8170, 1051.8807, 1087.1163,
+        1122.3405, 1157.5697, 1192.7893, 1228.0109, 1262.5994, 1298.7347,
+        1333.8929, 1369.2504, 1404.4696, 1437.5973, 1466.4719, 1489.6658,
+        1506.9510, 1519.3611, 1529.6740, 1536.5571, 1542.2289, 1553.9817,
+        1567.5085, 1581.7065, 1595.7616, 1608.4001, 1618.5583, 1625.8043,
+        1630.7007, 1635.0726, 1642.2443, 1648.8663, 1653.5099, 1659.0856,
+        1664.8807, 1670.4767, 1676.0770, 1681.6744, 1687.2720, 1692.8679,
+        1697.9827, 1705.2157, 1708.4655, 1714.3206, 1719.9102, 1730.0022,
+        1736.3169, 1726.1902, 1718.9109, 1723.3282, 1726.9940, 1730.0922,
+        1730.0206, 1730.9454, 1724.4452, 1720.3300, 1714.0845, 1721.4249,
+        1740.4800, 1748.4839, 1750.7720, 1756.2915, 1758.6555, 1761.3207,
+        1763.0050, 1764.2776, 1763.2791, 1765.4841, 1766.7526, 1768.1652,
+        1769.6892, 1771.3467, 1772.9883, 1774.1620, 1774.8845, 1775.6278]
+
+
+    HYPERVIEW_STD = [ 134.5265,  134.4990,  133.3613,  135.3228,  137.9043,  142.7918,
+         145.7327,  146.1628,  147.1680,  148.4370,  150.4806,  153.3336,
+         155.5741,  160.6224,  165.5196,  168.2650,  171.5105,  174.7307,
+         177.9374,  178.5810,  181.5402,  186.8667,  194.4707,  202.7640,
+         207.6506,  210.6972,  214.7988,  217.4728,  221.3656,  224.1183,
+         227.7696,  230.5495,  232.6815,  233.8724,  234.2247,  234.9455,
+         237.6151,  239.5447,  242.4203,  245.5482,  248.7913,  250.6793,
+         252.2389,  253.2567,  255.5542,  258.0208,  259.4061,  259.4346,
+         260.7409,  262.4182,  264.6888,  266.1444,  266.7072,  266.3409,
+         267.3909,  268.7917,  270.7439,  276.0544,  279.1968,  279.0555,
+         277.5002,  275.4283,  274.8527,  275.9222,  279.4376,  278.4554,
+         279.2577,  282.9919,  287.4024,  295.2889,  302.8371,  310.3907,
+         319.6028,  332.3335,  349.7060,  371.7979,  399.6372,  430.9706,
+         465.1447,  501.5895,  539.8292,  579.5327,  619.6236,  662.1959,
+         705.7219,  749.7064,  792.8994,  832.2605,  864.7523,  888.6902,
+         904.2859,  913.5572,  919.9628,  924.5461,  928.2289,  935.7452,
+         944.3081,  952.3723,  959.6835,  965.5954,  969.7115,  972.0714,
+         973.2435,  974.3464,  976.9781,  979.2787,  980.4351,  982.0870,
+         983.8807,  985.5811,  987.2900,  989.0009,  990.7166,  992.4350,
+         993.9419,  996.6221,  996.9525,  998.7151, 1000.3409, 1004.9735,
+        1008.0629, 1002.0215,  997.7035, 1000.1381, 1002.0745, 1003.5467,
+        1003.1268, 1003.3409,  999.4595,  996.8981,  992.7678,  996.0699,
+        1006.6043, 1011.1505, 1012.5848, 1015.7120, 1016.4387, 1016.4833,
+        1015.0517, 1012.7045, 1008.7697, 1006.6260, 1003.9716, 1001.2909,
+         998.6447,  995.7966,  992.4855,  988.7433,  984.6328,  980.2321]
     
 
     # Label stats
@@ -105,6 +107,8 @@ class HyperviewBenchmark(NonGeoDataset):
         transforms: Optional[Callable] = None,
         normalize: bool = True,
         seed: int = 13, #optional, for splitting
+        do_mask: bool = True, # to mask or not to mask
+        mask_value: float = 0.0, #for masking out the masked values
     ) -> None:
         assert split in self.valid_splits, (
             f"Only supports one of {self.valid_splits}, but found {split}."
@@ -115,6 +119,8 @@ class HyperviewBenchmark(NonGeoDataset):
         self.img_path = os.path.join(self.root, data_dir)
         self.splits_path = os.path.join(self.root, self.gt_file_path.format(self.split))
         self.transforms = transforms
+        self.do_mask = do_mask
+        self.mask_value = mask_value
         #check if the img_path exists
         if create_splits:
             self.seed = seed
@@ -213,8 +219,12 @@ class HyperviewBenchmark(NonGeoDataset):
         if self.normalize:
             image = self.channelwise_transforms(image)
 
+        if self.do_mask:
+            bool_mask = mask.bool().expand_as(image)
+            image = torch.masked_fill(image, bool_mask, self.mask_value)
+
         if self.transforms is not None:
-            image = self.transforms((image, mask)) # the first transform is the mask tensor
+            image = self.transforms(image) # the first transform is the mask tensor
 
         return image, label
 
@@ -254,19 +264,16 @@ class HyperviewBenchmark(NonGeoDataset):
 
     
 class RegDataAugmentation(torch.nn.Module):
-    def __init__(self, size, source_chn_ids, split="val", 
+    def __init__(self, size, source_chn_ids, split="train", 
                  mean=None, std=None, band_ids=None, 
-                 target_chn_ids=None, mask_image=True):
+                 target_chn_ids=None):
         super().__init__()
 
         flipH = K.RandomHorizontalFlip(p=0.5, keepdim=True)
         flipV = K.RandomVerticalFlip(p=0.5, keepdim=True)
         rcrop = K.RandomResizedCrop(_to_tuple(size), scale=(0.8, 1.0), keepdim=True) #, resample='bicubic')
         r = K.Resize(_to_tuple(size), keepdim=True)
-        if mask_image:
-            mask_tensor = MaskTensor(mask=mask_image)
-        else:
-            mask_tensor = None
+
         self.output_chn_ids = None
         
         # setup HS specific augmentations
@@ -283,8 +290,7 @@ class RegDataAugmentation(torch.nn.Module):
             self.output_chn_ids = source_chn_ids
 
         self.transforms = []
-        if mask_tensor is not None:
-            self.transforms.append(mask_tensor)
+
 
         if split == "train":
             if band_ids is not None:
@@ -322,11 +328,12 @@ class HyperviewDataset(BaseDataset):
     def __init__(self, config):
         super().__init__(config)
         self.mask_image = config.get("mask_image", True)
+        self.mask_value = config.get("mask_value", 0.0)
 
        
     def create_dataset(self):
-        train_transform = RegDataAugmentation(split="train", size=self.img_size, band_ids=self.band_ids, source_chn_ids=self.source_chn_ids, target_chn_ids=self.target_chn_ids, mask_image=self.mask_image)
-        eval_transform  = RegDataAugmentation(split="test", size=self.img_size, band_ids=self.band_ids, source_chn_ids=self.source_chn_ids, target_chn_ids=self.target_chn_ids, mask_image=self.mask_image)
+        train_transform = RegDataAugmentation(split="train", size=self.img_size, band_ids=self.band_ids, source_chn_ids=self.source_chn_ids, target_chn_ids=self.target_chn_ids)
+        eval_transform  = RegDataAugmentation(split="test", size=self.img_size, band_ids=self.band_ids, source_chn_ids=self.source_chn_ids, target_chn_ids=self.target_chn_ids)
 
 
         # Override the config with the transformed channel ids
@@ -336,13 +343,13 @@ class HyperviewDataset(BaseDataset):
             self.config['wavelengths_sigma_nm'] = output_chn_ids[:,1].tolist()
 
         dataset_train = HyperviewBenchmark(
-            root=self.root_dir, split="train", transforms=train_transform,
+            root=self.root_dir, split="train", transforms=train_transform, do_mask=self.mask_image, mask_value=self.mask_value
         )
         dataset_val = HyperviewBenchmark(
-            root=self.root_dir, split="val",  transforms=eval_transform,
+            root=self.root_dir, split="val",  transforms=eval_transform, do_mask=self.mask_image, mask_value=self.mask_value
         )
         dataset_test = HyperviewBenchmark(
-            root=self.root_dir, split="test",  transforms=eval_transform,
+            root=self.root_dir, split="test",  transforms=eval_transform, do_mask=self.mask_image, mask_value=self.mask_value
         )
 
         return dataset_train, dataset_val, dataset_test

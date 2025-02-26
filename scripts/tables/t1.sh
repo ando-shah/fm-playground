1d7b9b9e7a943c0cc2858bbb3257a2f0876da523
lewaldm <leonard.waldmann@gmx.de>
2025-02-26 19:48:24 +0100

    scripts

--- scripts/tables/t1.sh
+++ scripts/tables/t1.sh
@@ -3,16 +3,14 @@
 #SBATCH --mail-user=leonard.waldmann@tum.de
 ##SBATCH --output=/home/hk-project-pai00028/tum_mhj8661/code/slurm-%A_%a.out
 
-#SBATCH --job-name=eval
+#SBATCH --job-name=t1
 #SBATCH --partition=accelerated
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=20        # default: 38
-#SBATCH --time=1:30:00
-#SBATCH --array=0-2,8-15
-
-task_id=$SLURM_ARRAY_TASK_ID
+#SBATCH --time=4:00:00
+#SBATCH --array=12-15
 
 ##### export env variables
 export $(cat /home/hk-project-pai00028/tum_mhj8661/code/fm-playground/.env) # horeka
@@ -20,34 +18,50 @@ export $(cat /home/hk-project-pai00028/tum_mhj8661/code/fm-playground/.env) # ho
 
 
 fastdevrun=no
-exp_base_name=r02
+exp_base_name=t01
 
 all_tasks=(
 
-    # resisc45 (50e = 1h30 (lp))
-    'base/dofa linear_probe resisc45 900'
-    'base/dinov2 linear_probe resisc45 900'
-    'base/panopticon linear_probe resisc45 400'
+    # low prio: fmow-rgb, so2sat
 
-    # eurosat (50e = 0h15 (lp))
-    'base/croma_s2 linear_probe geobench_eurosat_12b 900'
-    'base/dofa linear_probe geobench_eurosat_13b 900'
-    'base/dinov2 linear_probe geobench_eurosat_rgb 900'
-    'base/softcon_13b linear_probe geobench_eurosat_13b 900'
-    'base/panopticon linear_probe geobench_eurosat_13b 200'
+    # m-eurosat (50e = 0h15 (lp))
+    # 'base/croma_s2 linear_probe geobench_eurosat_12b 900'
+    # 'base/dofa linear_probe geobench_eurosat_13b 900'
+    # 'base/dinov2 linear_probe geobench_eurosat_rgb 900'
+    # 'base/softcon_13b linear_probe geobench_eurosat_13b 900'
+    # 'base/panopticon_v2 linear_probe geobench_eurosat_13b 200'
 
     # benv2-s2 (50e, 10% subset = 0h20)
     'base/croma_s2 linear_probe benv2_s2_12b 900'
     'base/dofa linear_probe benv2_s2_12b 900'
     'base/dinov2 linear_probe benv2_s2_rgb 900'
-    'base/panopticon linear_probe benv2_s2_12b 200'
+    'base/panopticon_v2 linear_probe benv2_s2_12b 200'
+    'base/senpamae linear_probe benv2_s2_4b 900'
+
+    # resisc45 (50e = 1h30 (lp))
+    'base/dofa linear_probe resisc45 900'
+    'base/dinov2 linear_probe resisc45 900'
+    'base/panopticon_v2 linear_probe resisc45 400'
 
     # benv2-s1 
     'base/croma_s1 linear_probe benv2_s1 900'
     'base/dofa linear_probe benv2_s1 900'
     'base/softcon_2b linear_probe benv2_s1 900'
-    'base/panopticon linear_probe benv2_s1 400'
+    'base/panopticon_v2 linear_probe benv2_s1 400'
 
+    # fmow-wv
+
+    # forestnet
+
+
+    # corine-sd (previous t1)
+    'base/dofa linear_probe corine_sd 900'
+    'base/panopticon_v2 linear_probe corine_sd 200'
+    # 'base/senpamae linear_probe corine_sd_4b 900'
+
+    # corine-21 (previous t1)
+    'base/dofa linear_probe corine_21b 900'
+    'base/panopticon_v2 linear_probe corine_21b 130'
 )
 
 
@@ -56,6 +70,7 @@ all_tasks=(
 n_last_blocks_list='[1,4]'
 pooling='[avgpool,cls,default]'
 lrs_linear_probe='[1e-5,5e-5,1e-4,5e-4,1e-3,5e-3,1e-2,5e-2,0.1,0.5,1,3,5,10,20]'
+optim=adamw
 
 ########## pe linear probe (=partial finetune) defaults
 
@@ -64,81 +79,104 @@ warmup_epochs=0
 
 ########## defaults both
 
-epochs=50
-num_workers=8
-check_val_every_n_epoch=100
+epochs=100
+num_workers=16
+check_val_every_n_epoch=15
+val_subset=0.25
 
+# epochs=10
+# num_workers=12
+# check_val_every_n_epoch=1
+# val_subset=0.1
 
-########## extract task specific parameters
+########## get tasks
 
-task=${all_tasks[$task_id]}
-echo "Running Task: $task"
+if [ $# -eq 0 ]; then
+    if [ -n "$SLURM_ARRAY_TASK_ID" ]; then
+        task_ids=($SLURM_ARRAY_TASK_ID)
+    else
+        task_ids=($(seq 0 $((${#all_tasks[@]}-1))))
+    fi
+else
+    task_ids=("$@")
+fi
 
-set -- $task
-model=$1
-training_mode=$2
-ds=$3
-batch_size=$4
 
 ########## execution
 
-cmd="$PY_EXECUTABLE $REPO_PATH/geofm_src/main.py \
-    model=$model \
-    dataset=$ds \
-    output_dir=$ODIR/$exp_base_name/$ds/$model/$training_mode/ \
-    +model.training_mode=$training_mode \
-    ++batch_size=$batch_size \
-    num_workers=$num_workers \
-    num_gpus=1 \
-    seed=21 \
-    "
+for task_id in "${task_ids[@]}"; do
 
-if [ $fastdevrun == 'no' ]; then
-    cmd="$cmd epochs=$epochs batch_size=$batch_size trainer.check_val_every_n_epoch=$check_val_every_n_epoch"
+    task=${all_tasks[$task_id]}
+    echo "Running Task: $task"
 
-elif [ $fastdevrun == 'fast' ]; then
-    echo "fastdevrun 'fast'!"
-    cmd="$cmd epochs=1 batch_size=32 trainer.check_val_every_n_epoch=1"
-    cmd="$cmd dataset.subset.train=64 dataset.subset.val=64 dataset.subset.test=64"
-    lrs_partial_ft="0.1"
-
-elif [ $fastdevrun == 'bsz' ]; then
-    echo "fastdevrun 'bsz'!"
-    cmd="$cmd epochs=1 batch_size=$batch_size trainer.check_val_every_n_epoch=1"
-    s=$((batch_size * 1))
-    echo $s
-    cmd="$cmd dataset.subset.train=$s dataset.subset.val=$s dataset.subset.test=$s"
-    lrs_partial_ft="0.1"
-
-else
-    echo "fastdevrun not recognized"
-    exit 1
-fi
+    set -- $task
+    model=$1
+    training_mode=$2
+    ds=$3
+    batch_size=$4
 
 
-if [ $training_mode == 'linear_probe' ]; then
-    
-    cmd="$cmd \
-        +n_last_blocks_list=$n_last_blocks_list \
-        +pooling=$pooling \
-        +lr=$lrs_linear_probe"
-    echo $cmd
-    $cmd
+    cmd="$PY_EXECUTABLE $REPO_PATH/geofm_src/main.py \
+        model=$model \
+        dataset=$ds \
+        output_dir=$ODIR/$exp_base_name/$ds/$model/$training_mode/ \
+        +model.training_mode=$training_mode \
+        ++batch_size=$batch_size \
+        num_workers=$num_workers \
+        num_gpus=1 \
+        seed=21 \
+        "
+
+    if [ $fastdevrun == 'no' ]; then
+        cmd="$cmd epochs=$epochs batch_size=$batch_size trainer.check_val_every_n_epoch=$check_val_every_n_epoch dataset.subset.val=$val_subset"
+
+    elif [ $fastdevrun == 'fast' ]; then
+        echo "fastdevrun 'fast'!"
+        cmd="$cmd epochs=1 batch_size=32 trainer.check_val_every_n_epoch=1"
+        cmd="$cmd dataset.subset.train=64 dataset.subset.val=64 dataset.subset.test=64"
+        lrs_partial_ft="0.1"
+
+    elif [ $fastdevrun == 'bsz' ]; then
+        echo "fastdevrun 'bsz'!"
+        cmd="$cmd epochs=1 batch_size=$batch_size trainer.check_val_every_n_epoch=1"
+        s=$((batch_size * 1))
+        echo $s
+        cmd="$cmd dataset.subset.train=$s dataset.subset.val=$s dataset.subset.test=$s"
+        lrs_partial_ft="0.1"
+
+    else
+        echo "fastdevrun not recognized"
+        exit 1
+    fi
+
+
+    if [ $training_mode == 'linear_probe' ]; then
         
-elif [ $training_mode == 'partial_finetune' ]; then
-
-    for lr in $lrs_partial_ft; do
-        echo "partial finetune with lr=$lr"
-        lr_cmd="$cmd \
-            +lr=$lr \
-            +model.params_to_train=[] \
-            warmup_epochs=$warmup_epochs \
+        cmd="$cmd \
+            +n_last_blocks_list=$n_last_blocks_list \
+            +pooling=$pooling \
+            +lr=$lrs_linear_probe
+            +optim=\${_optims.$optim} \
             "
-        echo $lr_cmd
-        $lr_cmd
-    done
-fi
-        
+        echo $cmd
+        $cmd
+            
+    elif [ $training_mode == 'partial_finetune' ]; then
 
-# collect results
-$PY_EXECUTABLE $REPO_PATH/geofm_src/collect_results.py $ODIR/$exp_base_name/
+        for lr in $lrs_partial_ft; do
+            echo "partial finetune with lr=$lr"
+            lr_cmd="$cmd \
+                +lr=$lr \
+                +model.params_to_train=[] \
+                warmup_epochs=$warmup_epochs \
+                "
+            echo $lr_cmd
+            $lr_cmd
+        done
+    fi
+            
+
+    # collect results
+    $PY_EXECUTABLE $REPO_PATH/geofm_src/collect_results.py $ODIR/$exp_base_name/
+
+done

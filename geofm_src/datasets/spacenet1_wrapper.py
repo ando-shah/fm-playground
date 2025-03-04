26f8adc55979fa869afb3054e488145122762825
ando shah <ando@berkeley.edu>
2025-03-03 21:49:49 -0800

    spacenet subsetting channels

--- geofm_src/datasets/spacenet1_wrapper.py
+++ geofm_src/datasets/spacenet1_wrapper.py
@@ -8,12 +8,14 @@ import torch
 from torch import Tensor
 from .spacenet import SpaceNet1
 from .base_dataset import BaseDataset
-
+from .utils.utils import ChannelSampler
 
 class SegDataAugmentation(torch.nn.Module):
-    def __init__(self, mean, std, size, split="valid"):
+    def __init__(self, mean, std, size, split="valid", band_ids=None, source_chn_ids=None):
         super().__init__()
 
+        self.band_ids = band_ids
+            
         self.norm = K.augmentation.Normalize(mean=mean, std=std)
 
         if split == "train":
@@ -33,6 +35,9 @@ class SegDataAugmentation(torch.nn.Module):
     def forward(self, x, y):
         x = self.norm(x)
         x_out, y_out = self.transform(x, y)
+        if self.band_ids is not None:
+            #subset the channels
+            x_out = x_out[:, self.band_ids, :, :]
         return x_out, y_out
 
 
@@ -41,8 +46,8 @@ class SegGeoBenchTransform(object):
     STD = torch.tensor([410.9821842645062, 466.7364072457086, 534.92315090301, 618.9212492576934, 647.7404624683102, 819.0946643361298, 1225.529915856995, 1294.781212164937])
 
 
-    def __init__(self, split, size):
-        self.transform = SegDataAugmentation(mean=self.MEAN, std=self.STD, size=size, split=split)
+    def __init__(self, split, size, band_ids=None):
+        self.transform = SegDataAugmentation(mean=self.MEAN, std=self.STD, size=size, split=split, band_ids=band_ids)
 
     def __call__(self, sample):
         array, mask = self.transform(sample['image'].unsqueeze(0), sample['mask'].unsqueeze(0).unsqueeze(0))
@@ -58,14 +63,24 @@ class SpaceNet1Dataset(BaseDataset):
         self.return_rgb = config.get('return_rgb', False)
         self.num_channels = config.get('num_channels', 4)
         if self.num_channels == 3:
-            self.image = '3band'
+            # self.image = '3band'
+            self.image = '8band' #hacking for now to subset instead of rgb
+            self.band_ids = [4, 2, 1]
         elif self.num_channels == 8:
             self.image = '8band'
+            self.band_ids = None
 
     
     def create_dataset(self):
-        train_transform = SegGeoBenchTransform(split="train", size=self.img_size)
-        eval_transform = SegGeoBenchTransform(split="test", size=self.img_size)
+        train_transform = SegGeoBenchTransform(split="train", size=self.img_size, band_ids=self.band_ids)
+        eval_transform = SegGeoBenchTransform(split="test", size=self.img_size, band_ids=self.band_ids)
+
+        print(self.config['wavelengths_mean_nm'])
+
+        #index into the list self.config['wavelengths_mean_nm'] using the band_ids
+        if self.band_ids is not None:
+            self.config['wavelengths_mean_nm'] = [self.config['wavelengths_mean_nm'][i] for i in self.band_ids]
+            self.config['wavelengths_sigma_nm'] = [self.config['wavelengths_sigma_nm'][i] for i in self.band_ids]
 
         dataset_train = SpaceNet1(
             root=self.root_dir, split="train", transforms=train_transform, image=self.image)
